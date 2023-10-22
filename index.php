6796fba3a564be047bd1b291677dd9a4d8322757
Wilmer Garcia <104747642+ZequiGarcia@users.noreply.github.com>
2023-10-22 12:50:43 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Calculadora Creativa</title>
+    <title>Calculadora Móvil</title>
     <style>
         body {
             font-family: Arial, sans-serif;
@@ -9,82 +9,77 @@
             background-color: #f5f5f5;
         }
         #calculator {
-            width: 400px;
+            width: 300px;
             margin: 0 auto;
             background-color: #fff;
             padding: 20px;
             border-radius: 10px;
             box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.2);
         }
-        input[type="text"], input[type="number"] {
+        input[type="text"] {
             width: 100%;
             padding: 10px;
             margin: 10px 0;
             border: 1px solid #ccc;
             border-radius: 5px;
         }
-        button {
-            width: 50px;
-            height: 50px;
-            font-size: 18px;
-            margin: 5px;
-            border: none;
+        .button-container {
+            display: grid;
+            grid-template-columns: repeat(4, 1fr);
+            gap: 5px;
+        }
+        .button {
+            font-size: 20px;
+            padding: 10px;
+            border: 1px solid #ccc;
             border-radius: 5px;
             background-color: #3498db;
             color: #fff;
             cursor: pointer;
         }
-        button:hover {
+        .button:hover {
             background-color: #2980b9;
         }
     </style>
 </head>
 <body>
     <div id="calculator">
-        <h2>Calculadora Creativa</h2>
+        <h2>Calculadora Móvil</h2>
         <form method="post" action="">
-            <input type="number" name="num1" required>
-            <select name="operator">
-                <option value="add">+</option>
-                <option value="subtract">-</option>
-                <option value="multiply">*</option>
-                <option value="divide">/</option>
-            </select>
-            <input type="number" name="num2" required>
-            <button type="submit">Calcular</button>
+            <input type="text" name="display" id="display" readonly>
+            <div class="button-container">
+                <button class="button" type="button" onclick="appendToDisplay('7')">7</button>
+                <button class="button" type="button" onclick="appendToDisplay('8')">8</button>
+                <button class="button" type="button" onclick="appendToDisplay('9')">9</button>
+                <button class="button" type="button" onclick="appendToDisplay('+')">+</button>
+                <button class="button" type="button" onclick="appendToDisplay('4')">4</button>
+                <button class="button" type="button" onclick="appendToDisplay('5')">5</button>
+                <button class="button" type="button" onclick="appendToDisplay('6')">6</button>
+                <button class="button" type="button" onclick="appendToDisplay('-')">-</button>
+                <button class="button" type="button" onclick="appendToDisplay('1')">1</button>
+                <button class="button" type="button" onclick="appendToDisplay('2')">2</button>
+                <button class="button" type="button" onclick="appendToDisplay('3')">3</button>
+                <button class="button" type="button" onclick="appendToDisplay('*')">*</button>
+                <button class="button" type="button" onclick="clearDisplay()">C</button>
+                <button class="button" type="button" onclick="appendToDisplay('0')">0</button>
+                <button class="button" type="submit">=</button>
+                <button class="button" type="button" onclick="appendToDisplay('/')">/</button>
+            </div>
         </form>
-        <br>
+        <script>
+            function appendToDisplay(value) {
+                document.getElementById("display").value += value;
+            }
+
+            function clearDisplay() {
+                document.getElementById("display").value = '';
+            }
+        </script>
         <?php
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            $num1 = $_POST["num1"];
-            $num2 = $_POST["num2"];
-            $operator = $_POST["operator"];
-            $result = 0;
-
-            switch ($operator) {
-                case "add":
-                    $result = $num1 + $num2;
-                    break;
-                case "subtract":
-                    $result = $num1 - $num2;
-                    break;
-                case "multiply":
-                    $result = $num1 * $num2;
-                    break;
-                case "divide":
-                    if ($num2 != 0) {
-                        $result = $num1 / $num2;
-                    } else {
-                        echo "¡Error! No se puede dividir por cero.";
-                    }
-                    break;
-                default:
-                    echo "Operador no válido.";
-            }
-
-            if ($operator != "divide") {
-                echo "Resultado: $num1 $operator $num2 = $result";
-            }
+            $expression = $_POST["display"];
+            $result = eval("return $expression;");
+            echo "Resultado: $expression = $result";
         }
         ?>
     </div>
